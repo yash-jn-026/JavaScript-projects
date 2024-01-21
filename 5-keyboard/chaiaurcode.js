a1956f34a2769d1e0e3280854aa7abf906be51c1
yash jain <141411221+yash-jn-026@users.noreply.github.com>
2024-01-21 11:19:22 +0000

    push

--- 5-keyboard/chaiaurcode.js
+++ 5-keyboard/chaiaurcode.js
@@ -1,1 +1,22 @@
-console.log('Project 5');
+const insert = document.getElementById('insert')
+
+window.addEventListener('keydown',(e)=>{
+    insert.innerHTML = `
+    <div class= "color">
+    <table>
+  <tr>
+    <th>key </th>
+    <th>keycode</th>
+    <th>code</th>
+  </tr>
+  <tr>
+    <td>${e.key === ' ' ? "SPACE" :e.key}</td>
+    <td>${e.keyCode}</td>
+    <td>${e.code}</td>
+  </tr>
+  
+</table>
+    </div>
+    
+    `
+})
